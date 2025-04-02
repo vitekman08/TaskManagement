5e981596f277006c3843b955b15b45235e54ef6a
Viktor Bovaldykov <126251900+vitekman08@users.noreply.github.com>
2025-04-02 13:29:05 +0300

    Delete src directory

--- src/main/java/com/task/management/repository/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package com.task.management.repository;
-
-import com.task.management.domain.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TaskRepository extends JpaRepository<Task, Long> {
-}
